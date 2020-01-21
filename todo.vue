60367899288e84ece4592ffb0b1e8b7dc648517c
worranittha <baby_mind2@hotmail.com>
2020-01-21 14:33:17 +0700

    :recycle: remove comment

--- todo.vue
+++ todo.vue
@@ -1,29 +1,21 @@
 <template>
     <div>
-       <p><h3>To do list: </h3><p/>
-       Date <input type="text">  To do <input type="text"><br><br>
-
-       <!--create button-->
-       <!-- <button class="btn btn-success"  v-on:click ="SaveData"> Save Data </button>  -->
-       <!-- <button class="btn btn-info" v-on:click ="LoadData()"> Render table </button><br><br> -->
-
         <table class="table">
             <thead class="thead-dark">
                 <tr>
-                <th scope="col">task</th>
-                <th scope="col">Time</th>
-                <th scope="col">Done</th>
-               
+                    <th scope="col">task</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Done</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(ls) in list" v-bind:key="ls.id"> 
                 <!-- <th scope="row">{{1}}</th> -->
-                <td>{{ls.task}}</td>
-                <td>{{ls.time}}</td>
-                <td>
-                     <button class="btn btn-info"  > Done </button>
-                </td>
+                    <td>{{ls.task}}</td>
+                    <td>{{ls.time}}</td>
+                    <td>
+                        <button class="btn btn-info"  > Done </button>
+                    </td>
                 </tr>
             </tbody>
         </table>      
@@ -35,21 +27,22 @@ export default {
     data() {  //attribute
         return {
             list: [
-                {   time : "Monday 09:30 am",
+                {   
+                    time : "Monday 09:30 am",
                     task : "Big cleaning"
                 },
-                {   time : "Tuesday 13.00 pm",
+                {   
+                    time : "Tuesday 13.00 pm",
                     task : "Shopping"
                 },
-                 {  time : "Wednesday 14:00 pm",
+                 {  
+                     time : "Wednesday 14:00 pm",
                     task : "meeting"
                 }
             ]
         }
-    }, 
-
- } 
-
+    }
+} 
 </script>
 
 <style>
