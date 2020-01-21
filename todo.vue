70580cd108a8e4eac42d29591010c617d3fc7105
worranittha <baby_mind2@hotmail.com>
2020-01-21 12:37:00 +0700

    :zap: show data list

--- todo.vue
+++ todo.vue
@@ -5,27 +5,24 @@
 
        <!--create button-->
        <!-- <button class="btn btn-success"  v-on:click ="SaveData"> Save Data </button>  -->
-        <button class="btn btn-info"  v-on:click ="LoadData"> Render table </button><br><br>
+       <!-- <button class="btn btn-info" v-on:click ="LoadData()"> Render table </button><br><br> -->
 
         <table class="table">
             <thead class="thead-dark">
                 <tr>
-                <th scope="col">id</th>
-                <th scope="col">date</th>
-                <th scope="col">todo</th>
-                <th scope="col">note</th>
-                 <th scope="col">button</th> 
+                <th scope="col">task</th>
+                <th scope="col">Time</th>
+                <th scope="col">Done</th>
+               
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(user, index) in users" v-bind:key="user.id"> 
-                <th scope="row">{{index+1}}</th>
-
-                <td>{{user.date}}</td>
-                <td>{{user.do}}</td>
-                <td>{{user.note}}</td>
+                <tr v-for="(ls) in list" v-bind:key="ls.id"> 
+                <!-- <th scope="row">{{1}}</th> -->
+                <td>{{ls.task}}</td>
+                <td>{{ls.time}}</td>
                 <td>
-                     <button class="btn btn-danger"  v-on:click ="remove(index)"> Delete </button>
+                     <button class="btn btn-info"  v-on:click ="remove(index)"> Done </button>
                 </td>
                 </tr>
             </tbody>
@@ -37,24 +34,22 @@
 export default { 
     data() {  //attribute
         return {
-            users: []
+            list: [
+                {   time : "Monday 09:30 am",
+                    task : "Big cleaning"
+                },
+                {   time : "Tuesday 13.00 pm",
+                    task : "Shopping"
+                },
+                 {  time : "Wednesday 14:00 pm",
+                    task : "meeting"
+                }
+            ]
         }
     }, 
-    methods: {
-        
-        LoadData(){
-            fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(json => {
-                this.users = json
-            })
-        },
-        remove(index){
-            this.users.splice(index ,1)
-        }
-    },
-    
-}
+
+ } 
+
 </script>
 
 <style>
