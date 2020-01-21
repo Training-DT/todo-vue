44b565cbf767ed507bede2f0d42f4cecc60942e5
Thanchanok Tipmaneekomut <thanchatip.coe@gmail.com>
2020-01-21 10:53:55 +0700

    :construction: Add todo list template

--- todo.vue
+++ todo.vue
@@ -1,5 +1,40 @@
 <template>
     <div>
-        
+        <h1 style = "color: blueviolet">Add to do list </h1>
+        Task : <input type="text"><br/><br/>
+        Due Date : <input type="text"><br/><br/>    
+        <button type="button" 
+                class="btn btn-info" 
+                v-on:click="saveTodo()">
+        Save</button><br/><br/> 
+        <p>{{this.taskTodo}}</p>
+        <p>{{this.dueDate}}</p>
     </div>
 </template>
+
+<script>
+
+    export default{
+        data(){
+            return{
+                taskTodo:"",
+                dueDate:"",
+            }
+        },
+        methods:{
+            saveTodo(){
+               this.taskTodo = document.body.children[0].children[1].value;
+               this.dueDate = document.body.children[0].children[4].value;
+              
+              // console.log("save !");
+              // console.log(this.dueDate);
+               }
+            }
+    }
+
+
+</script>
+
+<style>
+    @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css")
+</style>
